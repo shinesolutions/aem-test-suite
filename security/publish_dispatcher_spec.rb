2ae5767abf1957a294d8731b0dbcb095ad3df145
Ibrahim Hussein <ibrahim.hussein@shinesolutions.com>
2018-01-19 11:05:19 +1100

    added test to check secure routes

--- security/publish_dispatcher_spec.rb
+++ security/publish_dispatcher_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../spec_helper'
 
 describe 'Publish-Dispatcher', type: :feature do
+  # urlFile=File.new('secure_public_urls.txt','r');
+
   before :each do
     @conf = read_config
     init_dispatcher_client(@conf['aem']['dispatcher'])
@@ -14,5 +16,15 @@ describe 'Publish-Dispatcher', type: :feature do
   it 'should set up clickjacking prevention by setting X-Frame-Options header' do
     visit '/'
     expect(response_headers['X-Frame-Options']).to eq('SAMEORIGIN')
+  end
+
+  it 'should prevent access to secure routes' do
+    Dir.chdir(File.dirname(__FILE__))
+    secure_urls=File.readlines("secure_public_routes.txt")
+    #check each url
+    secure_urls.each do |url|
+      visit url
+      expect(page.status_code).to eq(404)
+    end
   end
 end
