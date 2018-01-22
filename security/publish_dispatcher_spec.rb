defd2749615d2a4936a00627cee33c1f3b30881c
Ibrahim Hussein <ibrahim.hussein@shinesolutions.com>
2018-01-22 11:02:49 +1100

    removed comments

--- security/publish_dispatcher_spec.rb
+++ security/publish_dispatcher_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../spec_helper'
 
 describe 'Publish-Dispatcher', type: :feature do
-
   before :each do
     @conf = read_config
     init_dispatcher_client(@conf['aem']['dispatcher'])
