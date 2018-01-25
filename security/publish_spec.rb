c19807e2834d6457f9191d7df44afece6a95ec54
Ibrahim Hussein <ibrahim.hussein@shinesolutions.com>
2018-01-25 13:29:37 +1100

    added test for access with default credentials #8

--- security/publish_spec.rb
+++ security/publish_spec.rb
@@ -3,9 +3,17 @@ require_relative '../spec_helper'
 describe 'Publish', type: :feature do
   before :each do
     @conf = read_config
+    @aem_publish = init_publish_client(@conf['aem']['publish'])
   end
 
   it 'should not be able to login using default admin password' do
+    # use aem api calls
+    aem = @aem_publish.aem
+    begin
+      aem.get_agents('publish')
+    rescue RubyAem::Error => error
+      # response should be unauthorized
+      expect(error.result.response.status_code).to eq(401)
+    end
   end
-
 end
