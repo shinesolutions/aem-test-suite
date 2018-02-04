e1c2ff45228e292701a85c5b044c106657413ed9
Ibrahim Hussein <ibrahim.hussein@shinesolutions.com>
2018-02-05 09:31:54 +1100

    updated publish default credentials check

--- security/publish_spec.rb
+++ security/publish_spec.rb
@@ -4,16 +4,18 @@ describe 'Publish', type: :feature do
   before :each do
     @conf = read_config
     @aem_publish = init_publish_client(@conf['aem']['publish'])
+
   end
 
   it 'should not be able to login using default admin password' do
     # use aem api calls
-    aem = @aem_publish.aem
+    bundle = @aem_publish.bundle('com.adobe.cq.social.cq-social-forum')
     begin
-      result = aem.get_agents('publish')
+      result = bundle.start
     rescue RubyAem::Error => error
       # response should be unauthorized
-      expect(error.result.response.status_code).to eq(401)
+      # checking for 500 since a NoAuthenticationHandlerException Java Exception is thrown, thus a server error
+      expect(error.result.response.status_code).to eq(500)
     end
     # no result due to error raised
     expect(result).to eq(nil)
