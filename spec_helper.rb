acb6ba12d1dec3d175fa20b31bf7398b16669a79
Michael Bloch <michael.bloch@shinesolutions.com>
2018-02-19 15:35:36 +1100

    Updated spec_helper requirements.

--- spec_helper.rb
+++ spec_helper.rb
@@ -21,7 +21,6 @@ require 'capybara/poltergeist'
 require 'phantomjs'
 require 'tempfile'
 require 'ruby_aem_aws'
-#require_relative '../ruby_aem_aws/lib/ruby_aem_aws'
 Dir.glob("spec/**/*.rb").each do |req_files|
   require_relative req_files
 end
